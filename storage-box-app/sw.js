72c75dc498868c1dd7c6fbc47169005f9c258bf8
HJ-Ghub <hwijung.park@gmail.com>
2026-06-20 00:20:01 +0900

    Improve storage box cloud sync

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v1';
+const CACHE_NAME = 'storage-box-app-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
