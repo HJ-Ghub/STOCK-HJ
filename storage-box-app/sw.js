e2cd49a2be33bc81468d79a84eb7357af7ba5b76
HJ-Ghub <hwijung.park@gmail.com>
2026-06-22 10:16:59 +0900

    Add storage item photo removal and move controls

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v3';
+const CACHE_NAME = 'storage-box-app-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
