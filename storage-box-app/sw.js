784a75b7e63238a980ae0d1e25c655e3e0c20437
HJ-Ghub <hwijung.park@gmail.com>
2026-06-22 10:48:35 +0900

    Move storage item editing into detail dialog

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v5';
+const CACHE_NAME = 'storage-box-app-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
