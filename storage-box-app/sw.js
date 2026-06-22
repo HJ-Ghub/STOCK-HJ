b4cb8902b95727191c4f790b48198a89f157953f
HJ-Ghub <hwijung.park@gmail.com>
2026-06-22 11:23:18 +0900

    Add mobile home visibility and app lock options

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v10';
+const CACHE_NAME = 'storage-box-app-v11';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
