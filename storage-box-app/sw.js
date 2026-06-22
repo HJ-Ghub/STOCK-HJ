f44579a348f1a146b94ba62f6d8b55346d13098a
HJ-Ghub <hwijung.park@gmail.com>
2026-06-22 10:52:09 +0900

    Fix storage edit dialog scrolling

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v6';
+const CACHE_NAME = 'storage-box-app-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
