90cfee61b390fcb084400f195c7e46e9e435e586
HJ-Ghub <hwijung.park@gmail.com>
2026-06-23 15:31:04 +0900

    Disable automatic storage AI analysis

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v17';
+const CACHE_NAME = 'storage-box-app-v18';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
