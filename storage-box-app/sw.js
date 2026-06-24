f90c1703ff95a49b8f2625d37d1322f7f1be1559
HJ-Ghub <hwijung.park@gmail.com>
2026-06-24 12:59:04 +0900

    Revert storage app before AI photo recognition

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v26';
+const CACHE_NAME = 'storage-box-app-v27';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
