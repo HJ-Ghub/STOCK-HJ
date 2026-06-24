8c6747a365ae26383de6219671bcf2961068a461
HJ-Ghub <hwijung.park@gmail.com>
2026-06-24 15:08:35 +0900

    Restore scrolling and add box photo viewer

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v35';
+const CACHE_NAME = 'storage-box-app-v36';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
