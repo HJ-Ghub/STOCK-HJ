69d89117ac3838c446663763668a68eeab769350
HJ-Ghub <hwijung.park@gmail.com>
2026-06-24 13:04:56 +0900

    Fix mobile photo picker trigger without AI

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v27';
+const CACHE_NAME = 'storage-box-app-v28';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
