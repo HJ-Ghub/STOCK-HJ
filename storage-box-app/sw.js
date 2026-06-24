b902270c56d6ba567ca7d5f3c4fb77e5b58ca079
HJ-Ghub <hwijung.park@gmail.com>
2026-06-24 14:57:36 +0900

    Fit storage app header to mobile width

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v34';
+const CACHE_NAME = 'storage-box-app-v35';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
