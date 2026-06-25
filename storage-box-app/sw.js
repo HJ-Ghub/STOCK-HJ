00044c6009ec7380b328321543d1a1a89fb6a607
HJ-Ghub <hwijung.park@gmail.com>
2026-06-25 13:13:20 +0900

    Add mouse wheel photo zoom

--- storage-box-app/sw.js
+++ storage-box-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'storage-box-app-v42';
+const CACHE_NAME = 'storage-box-app-v43';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
