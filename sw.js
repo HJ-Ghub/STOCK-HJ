f2956bf65a3ca31eba6e02a8a9a2516c870df0a6
HJ-Ghub <hwijung.park@gmail.com>
2026-07-02 11:40:15 +0900

    Allow resizing free input boxes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-journal-app-v20260702-freebox-move-delete1';
+const CACHE_NAME = 'stock-journal-app-v20260702-freebox-resize1';
 const CORE_ASSETS = [
   './',
   './index.html',
